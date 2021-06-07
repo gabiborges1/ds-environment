f3a4ff0da1851c7a51f450fb81f0289498fea268
Gabi <gabilimaborges@hotmail.com>
2021-06-07 09:06:04 -0300

    Add make

--- env-setup.sh
+++ env-setup.sh
@@ -28,11 +28,11 @@ for lib in $libs; do
 done
 
 # To avoid typing sudo whenever you run the docker command
-#sudo usermod -aG docker ${USER}
+sudo usermod -aG docker ${USER}
 
 # Docker-compose
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Installing anaconda
 which conda &> /dev/null
